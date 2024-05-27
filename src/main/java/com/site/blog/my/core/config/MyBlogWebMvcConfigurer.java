b7a9e8b64bbfd6452e53f7e2dadf3de9c09742c5
李艺儒 <11328742+L_Y_R_0215@user.noreply.gitee.com>
2024-05-28 01:17:03 +0800

    优化显示界面，新增视频接口

--- src/main/java/com/site/blog/my/core/config/MyBlogWebMvcConfigurer.java
+++ src/main/java/com/site/blog/my/core/config/MyBlogWebMvcConfigurer.java
@@ -25,5 +25,6 @@ public class MyBlogWebMvcConfigurer implements WebMvcConfigurer {
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**").addResourceLocations("file:" + Constants.FILE_UPLOAD_DIC);
+        registry.addResourceHandler("/video/**").addResourceLocations("file:"+Constants.VIDEO_UPLOAD_DIV);
     }
 }
